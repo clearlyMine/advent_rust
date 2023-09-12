7417369b7a86013f806ad1583bee4c1ec7c04ea0
clearly mine <clearlymine@pm.me>
2023-09-12 18:38:48 +0530

    update day 24 to increase efficiency

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -15,45 +15,95 @@ fn main() {
 
 fn process_part_1(input: &str) -> usize {
     let grid: BTreeMap<Coord, Tile> = parse_input(input);
-    // display_grid(&grid);
     let max_row = grid.iter().map(|(coord, _)| coord.row).max().unwrap();
-    // let max_col = grid.iter().map(|(coord, _)| coord.col).max().unwrap();
 
     const STARTING_POSITION: Coord = Coord { row: 0, col: 1 };
-    let destination: Vec<&Coord> = grid
+    let destination = grid
         .iter()
         .filter(|(coord, tile)| coord.row == max_row && **tile == Tile::Ground)
         .map(|(coord, _)| coord)
-        .collect();
-    if destination.len() > 1 {
-        panic!("More than 1 destinations");
-    }
-    let destination = destination.iter().last().unwrap();
-    // dbg!(starting_position);
-    // dbg!(destination);
-    find_path_bfs(&grid, &STARTING_POSITION, *destination, 0)
+        .last()
+        .unwrap();
+    find_path_bfs(&grid, &STARTING_POSITION, destination, 0)
 }
 
 fn process_part_2(input: &str, time_to_destination: usize) -> usize {
     let grid: BTreeMap<Coord, Tile> = parse_input(input);
-    // display_grid(&grid);
     let max_row = grid.iter().map(|(coord, _)| coord.row).max().unwrap();
-    // let max_col = grid.iter().map(|(coord, _)| coord.col).max().unwrap();
 
     const STARTING_POSITION: Coord = Coord { row: 0, col: 1 };
-    let destination: Vec<&Coord> = grid
+    let destination = grid
         .iter()
         .filter(|(coord, tile)| coord.row == max_row && **tile == Tile::Ground)
         .map(|(coord, _)| coord)
-        .collect();
-    if destination.len() > 1 {
-        panic!("More than 1 destinations");
+        .last()
+        .unwrap();
+    let back_to_start = find_path_bfs(&grid, destination, &STARTING_POSITION, time_to_destination);
+    find_path_bfs(&grid, &STARTING_POSITION, destination, back_to_start)
+}
+
+fn find_path_bfs(
+    grid: &BTreeMap<Coord, Tile>,
+    starting_position: &Coord,
+    destination: &Coord,
+    starting_round: usize,
+) -> usize {
+    let mut queue: VecDeque<State> = VecDeque::new();
+    let initial_state = State::new(*starting_position, starting_round, 0);
+    queue.push_front(initial_state);
+
+    let max_row = grid.iter().map(|(coord, _)| coord.row).max().unwrap();
+    let max_col = grid.iter().map(|(coord, _)| coord.col).max().unwrap();
+
+    let max_states = lcm(max_row - 1, max_col - 1);
+    let mut states_processed: Vec<State> = vec![initial_state];
+
+    while let Some(state) = queue.pop_front() {
+        // dbg!(queue.len());
+        let current_position = state.current_position;
+        let round = state.round;
+        let (row, col) = (current_position.row, current_position.col);
+        for (nr, nc) in get_next_coords(row, col) {
+            if (nr, nc) == (destination.row, destination.col) {
+                return round + 1;
+            }
+            if (nr, nc) == (starting_position.row, starting_position.col) && state.wait < max_states
+            {
+                let new_state = State::new(
+                    Coord::new(nr as usize, nc as usize),
+                    round as usize + 1,
+                    state.wait + 1,
+                );
+                if !is_state_processed(&states_processed, &new_state, max_states) {
+                    queue.push_back(new_state);
+                    states_processed.push(new_state);
+                }
+                continue;
+            }
+            if nr < max_row && nc < max_col {
+                if no_blizzards(nr, nc, round, max_row, max_col, grid) {
+                    let wait = if (nr, nc) == (row, col) {
+                        state.wait + 1
+                    } else {
+                        0
+                    };
+                    if wait >= max_states {
+                        continue;
+                    }
+                    let new_state = State::new(
+                        Coord::new(nr as usize, nc as usize),
+                        round as usize + 1,
+                        wait,
+                    );
+                    if !is_state_processed(&states_processed, &new_state, max_states) {
+                        queue.push_back(new_state);
+                        states_processed.push(new_state);
+                    }
+                }
+            }
+        }
     }
-    let destination = destination.iter().last().unwrap();
-    // dbg!(starting_position);
-    // dbg!(destination);
-    let back_to_start = find_path_bfs(&grid, *destination, &STARTING_POSITION, time_to_destination);
-    find_path_bfs(&grid, &STARTING_POSITION, *destination, back_to_start)
+    0
 }
 
 #[derive(Clone, Debug, Copy)]
@@ -75,71 +125,66 @@ impl State {
     fn is_same(&self, other: &State, max_rounds: usize) -> bool {
         self.current_position == other.current_position
             && self.round % max_rounds == other.round % max_rounds
-            && self.wait == other.wait
     }
 }
 
-fn is_state_been_processed(states: &Vec<State>, to_compare: &State, max_rounds: usize) -> bool {
-    states
-        .iter()
-        .filter(|cur| cur.is_same(to_compare, max_rounds))
-        .collect::<Vec<_>>()
-        .len()
-        > 0
+fn is_state_processed(states: &Vec<State>, to_compare: &State, max_rounds: usize) -> bool {
+    states.iter().any(|cur| cur.is_same(to_compare, max_rounds))
 }
 
-fn find_path_bfs(
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    (a * b) / gcd(a, b)
+}
+
+fn get_next_coords(row: usize, col: usize) -> Vec<(usize, usize)> {
+    let mut out: Vec<(usize, usize)> = vec![(row + 1, col), (row, col + 1)];
+    if col > 0 {
+        out.push((row, col - 1));
+    }
+    if row > 0 {
+        out.push((row - 1, col));
+    }
+    out.push((row, col));
+    out
+}
+
+fn no_blizzards(
+    row: usize,
+    col: usize,
+    round: usize,
+    max_row: usize,
+    max_col: usize,
     grid: &BTreeMap<Coord, Tile>,
-    starting_position: &Coord,
-    destination: &Coord,
-    starting_round: usize,
-) -> usize {
-    let mut queue: VecDeque<State> = VecDeque::new();
-    queue.push_front(State::new(*starting_position, starting_round, 0));
-
-    let max_row = grid.iter().map(|(coord, _)| coord.row).max().unwrap();
-    let max_col = grid.iter().map(|(coord, _)| coord.col).max().unwrap();
-    //find lcm
-    fn gcd(a: usize, b: usize) -> usize {
-        if b == 0 {
-            a
-        } else {
-            gcd(b, a % b)
-        }
+) -> bool {
+    let c = Coord::new(row, col);
+    if !grid.contains_key(&c) {
+        return false;
     }
-
-    fn lcm(a: usize, b: usize) -> usize {
-        (a * b) / gcd(a, b)
+    if grid.get(&c).unwrap() == &Tile::Wall {
+        return false;
     }
+    let nr = row as isize;
+    let nc = col as isize;
+    let round = round as isize + 1;
+    let max_row = max_row - 1;
+    let max_col = max_col - 1;
 
-    let max_states = lcm(max_row - 1, max_col - 1);
-    let mut states_processed: Vec<State> = vec![];
-
-    while let Some(state) = queue.pop_front() {
-        // dbg!(queue.len());
-        let current_position = state.current_position;
-        let round = state.round;
-        // dbg!(round);
-        let wait = state.wait;
-        if current_position == *destination {
-            return round;
-        }
-        let round = round + 1;
-        current_position
-            .get_valid_neighbours(grid, round)
-            .iter()
-            .for_each(|c| {
-                let new_state = State::new(*c, round, 0);
-                if !is_state_been_processed(&states_processed, &new_state, max_states) {
-                    queue.push_back(new_state);
-                    states_processed.push(new_state);
-                }
-            });
-        if wait < max_states && current_position.is_valid(grid, round) {
-            queue.push_back(State::new(current_position, round, wait + 1));
-        }
-    }
-    panic!()
+    let east = &Coord::new(nr as usize, get_wrapped_count(nc - round, max_col));
+    let west = &Coord::new(nr as usize, get_wrapped_count(nc + round, max_col));
+    let south = &Coord::new(get_wrapped_count(nr - round, max_row), nc as usize);
+    let north = &Coord::new(get_wrapped_count(nr + round, max_row), nc as usize);
+    grid.get(east).unwrap() != &Tile::Blizzard(Direction::East)
+        && grid.get(west).unwrap() != &Tile::Blizzard(Direction::West)
+        && grid.get(south).unwrap() != &Tile::Blizzard(Direction::South)
+        && grid.get(north).unwrap() != &Tile::Blizzard(Direction::North)
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -167,86 +212,10 @@ impl Coord {
     fn new(row: usize, col: usize) -> Coord {
         Coord { row, col }
     }
-
-    fn neighbours(&self) -> Vec<Coord> {
-        let row = self.row;
-        let col = self.col;
-        let mut nebs = vec![];
-
-        //south
-        nebs.push(Coord { row: row + 1, col });
-        //east
-        nebs.push(Coord { row, col: col + 1 });
-        //west
-        if col > 0 {
-            nebs.push(Coord { row, col: col - 1 });
-        }
-        //north
-        if row > 0 {
-            nebs.push(Coord { row: row - 1, col });
-        }
-        nebs
-    }
-
-    fn get_valid_neighbours(&self, grid: &BTreeMap<Coord, Tile>, round: usize) -> Vec<Coord> {
-        let n = self.neighbours();
-        n.into_iter().filter(|x| x.is_valid(grid, round)).collect()
-    }
-
-    fn is_valid(&self, grid: &BTreeMap<Coord, Tile>, round: usize) -> bool {
-        let current_position = self;
-        if !grid.contains_key(&current_position) {
-            return false;
-        }
-        let tile = grid.get(&current_position).unwrap();
-        if *tile == Tile::Wall {
-            return false;
-        }
-        let round = round as isize;
-
-        let row = current_position.row as isize;
-        let col = current_position.col as isize;
-        //look around the grid current_position in every direction
-        let max_row = grid.iter().map(|(coord, _)| coord.row).max().unwrap() - 1;
-        let max_col = grid.iter().map(|(coord, _)| coord.col).max().unwrap() - 1;
-        let max_row = max_row;
-        let max_col = max_col;
-        let round = round as isize;
-
-        //from Up 'v'
-        let north_row = get_wrapped_count(max_row, row - round);
-        let north: Coord = Coord::new(north_row, col as usize);
-        if *grid.get(&north).unwrap() == Tile::Blizzard(Direction::South) {
-            return false;
-        }
-
-        //from Down '^'
-        let south_row = get_wrapped_count(max_row, row + round);
-        let south: Coord = Coord::new(south_row as usize, col as usize);
-        if *grid.get(&south).unwrap() == Tile::Blizzard(Direction::North) {
-            return false;
-        }
-
-        //from West '>'
-        let west_col = get_wrapped_count(max_col, col - round);
-        let west: Coord = Coord::new(row as usize, west_col as usize);
-        if *grid.get(&west).unwrap() == Tile::Blizzard(Direction::East) {
-            return false;
-        }
-
-        //from East '<'
-        let east_col = get_wrapped_count(max_col, col + round);
-        let east: Coord = Coord::new(row as usize, east_col as usize);
-        if *grid.get(&east).unwrap() == Tile::Blizzard(Direction::West) {
-            return false;
-        }
-
-        true
-    }
 }
 
-fn get_wrapped_count(max: usize, num: isize) -> usize {
-    if num < 1 as isize {
+fn get_wrapped_count(num: isize, max: usize) -> usize {
+    if num < 1 {
         let max = max as isize;
         let range = max;
         let num = num + range * ((1 - num) / range + 1);
@@ -255,31 +224,6 @@ fn get_wrapped_count(max: usize, num: isize) -> usize {
     let range = max;
     return (1 as isize + ((num - 1) % range as isize)) as usize;
 }
-
-// fn display_grid(grid: &BTreeMap<Coord, Tile>) {
-//     let max_row = grid.iter().map(|(coord, _)| coord.row).max().unwrap();
-//     let max_col = grid.iter().map(|(coord, _)| coord.col).max().unwrap();
-//     for row in 0..=max_row {
-//         for col in 0..=max_col {
-//             let coord = Coord { row, col };
-//             if let Some(tile) = grid.get(&coord) {
-//                 match tile {
-//                     Tile::Ground => print!("."),
-//                     Tile::Wall => print!("#"),
-//                     Tile::Blizzard(direction) => match direction {
-//                         Direction::North => print!("^"),
-//                         Direction::South => print!("v"),
-//                         Direction::East => print!(">"),
-//                         Direction::West => print!("<"),
-//                     },
-//                 }
-//             } else {
-//                 break;
-//             }
-//         }
-//         println!();
-//     }
-// }
 
 fn parse_input(input: &str) -> BTreeMap<Coord, Tile> {
     let mut grid: BTreeMap<Coord, Tile> = BTreeMap::new();
