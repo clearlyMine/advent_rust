ed8dcf6837f300c4b3974e3b47a40fefe7144d8c
clearly mine <clearlymine@pm.me>
2023-12-02 13:53:38 +0530

    optimise solution for 2023 day 02

--- year_2023/src/bin/day02.rs
+++ year_2023/src/bin/day02.rs
@@ -1,6 +1,9 @@
 use nom::bytes::complete::{tag, take_till};
+use nom::character::complete::{alpha1, digit1, space1};
 use nom::combinator::map_res;
-use nom::{character, *};
+use nom::multi::separated_list1;
+use nom::sequence::separated_pair;
+use nom::IResult;
 use std::time::Instant;
 
 fn main() {
@@ -30,19 +33,15 @@ impl Game {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-enum BallColor {
-    Red,
-    Blue,
-    Green,
-}
-
 fn process_part_1(input: &str) -> usize {
     input
         .lines()
         .enumerate()
         .map(|(i, line)| {
             let (_, games) = parse_line(line).unwrap();
+            (i, games)
+        })
+        .fold(0, |mut sum, (i, games)| {
             let (mut min_red, mut min_green, mut min_blue) = (0, 0, 0);
             for game in games {
                 min_red = min_red.max(game.red);
@@ -50,11 +49,10 @@ fn process_part_1(input: &str) -> usize {
                 min_green = min_green.max(game.green);
             }
             if min_red <= 12 && min_blue <= 14 && min_green <= 13 {
-                return i + 1;
+                sum += i + 1;
             }
-            0
+            sum
         })
-        .sum()
 }
 
 fn process_part_2(input: &str) -> u32 {
@@ -62,6 +60,9 @@ fn process_part_2(input: &str) -> u32 {
         .lines()
         .map(|line| {
             let (_, games) = parse_line(line).unwrap();
+            games
+        })
+        .map(|games| {
             let (mut min_red, mut min_green, mut min_blue) = (0, 0, 0);
             for game in games {
                 min_red = min_red.max(game.red);
@@ -76,60 +77,28 @@ fn process_part_2(input: &str) -> u32 {
 fn parse_line(input: &str) -> IResult<&str, Vec<Game>> {
     let (input, _) = take_till(|c| c == ':')(input)?;
     let (input, _) = tag(": ")(input)?;
-    let (input, games) = parse_games(input)?;
+    let (input, games) = separated_list1(tag("; "), parse_game)(input)?;
     Ok((input, games))
 }
 
-fn parse_games(input: &str) -> IResult<&str, Vec<Game>> {
-    let mut games: Vec<Game> = vec![];
-    let mut input = input;
-    loop {
-        let x;
-        (input, x) = bytes::complete::take_till(|c| c == ';')(input)?;
-        let (rest, g) = parse_balls(x)?;
-        if rest.len() != 0 {
-            panic!()
+fn parse_game(input: &str) -> IResult<&str, Game> {
+    let (input, balls) =
+        separated_list1(tag(", "), separated_pair(parse_number, space1, alpha1))(input)?;
+    let mut blue = 0;
+    let mut green = 0;
+    let mut red = 0;
+
+    for (count, color) in balls {
+        match color {
+            "blue" => blue = count,
+            "green" => green = count,
+            "red" => red = count,
+            _ => panic!("Unknown color: {}", color),
         }
-        let (mut red, mut green, mut blue) = (0, 0, 0);
-        for game in g {
-            match game.1 {
-                BallColor::Red => red = game.0,
-                BallColor::Blue => blue = game.0,
-                BallColor::Green => green = game.0,
-            }
-        }
-        let game: Game = Game::new(red, green, blue);
-        games.push(game);
-        if input.len() == 0 {
-            break;
-        }
-        (input, _) = tag("; ")(input)?;
     }
-    Ok((input, games))
+    Ok((input, Game::new(red, green, blue)))
 }
 
-fn parse_balls(input: &str) -> IResult<&str, Vec<(u32, BallColor)>> {
-    let mut balls: Vec<(u32, BallColor)> = vec![];
-    let mut input = input;
-    loop {
-        let number;
-        (input, number) = map_res(bytes::complete::take_till(|c| c == ' '), |s: &str| {
-            s.parse::<u32>()
-        })(input)?;
-        (input, _) = tag(" ")(input)?;
-        let ball_color;
-        (input, ball_color) = take_till(|c| c == ',')(input)?;
-        let ball = match ball_color {
-            "red" => BallColor::Red,
-            "blue" => BallColor::Blue,
-            "green" => BallColor::Green,
-            _ => panic!(),
-        };
-        balls.push((number, ball));
-        if input.len() == 0 {
-            break;
-        }
-        (input, _) = tag(", ")(input)?;
-    }
-    Ok((input, balls))
+fn parse_number(input: &str) -> IResult<&str, u32> {
+    map_res(digit1, |s: &str| s.parse::<u32>())(input)
 }
