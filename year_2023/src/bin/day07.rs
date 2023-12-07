590232e027ca8c1c279b897e79d884b65dcdf668
clearly mine <clearlymine@pm.me>
2023-12-07 19:50:40 +0530

    optimize output of 2023-day07
    This is done to make this the same output as all the other days so that it could be used later on during benchmarking and getting the timings of each day.

--- year_2023/src/bin/day07.rs
+++ year_2023/src/bin/day07.rs
@@ -5,12 +5,12 @@ fn main() {
         .output()
         .unwrap();
     let output = String::from_utf8(cmd.stdout).unwrap();
-    println!("{}", output);
+    print!("{}", output);
 
     let cmd = Command::new("cargo")
         .args(["run", "--release", "--bin", "day07_part2"])
         .output()
         .unwrap();
     let output = String::from_utf8(cmd.stdout).unwrap();
-    println!("{}", output);
+    print!("{}", output);
 }
