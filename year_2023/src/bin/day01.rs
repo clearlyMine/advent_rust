0442c6390a2224372609c568040775207b2c20b2
clearly mine <clearlymine@pm.me>
2023-12-07 20:38:19 +0530

    optimize solution for 2023-day01

--- year_2023/src/bin/day01.rs
+++ year_2023/src/bin/day01.rs
@@ -15,64 +15,44 @@ fn main() {
 }
 
 fn process_part_1(input: &str) -> u32 {
-    let mut numbers: Vec<u32> = vec![];
-    //this is faster than using functional programming here
-    for line in input.lines() {
-        let chars = line.chars();
-        let mut all_numbers: Vec<u32> = vec![];
-        for char in chars {
-            if char.is_numeric() {
-                all_numbers.push(char.to_digit(10).unwrap_or(0));
-            }
-        }
-        let x = all_numbers[0] * 10 + all_numbers[all_numbers.len() - 1];
-        numbers.push(x);
-    }
-    numbers.iter().sum()
+    input.lines().fold(0, |acc, line| {
+        let mut chars = line.chars();
+        let first = chars
+            .find(|x| x.is_numeric())
+            .expect("At least 1 number was expected");
+        let last = chars.rfind(|x| x.is_numeric()).unwrap_or(first);
+        let first = first.to_digit(10).unwrap();
+        let last = last.to_digit(10).unwrap();
+
+        acc + first * 10 + last
+    })
 }
 
 fn process_part_2(input: &str) -> u32 {
     input
         .lines()
         .map(|line| {
-            let n = [
-                "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2",
-                "3", "4", "5", "6", "7", "8", "9",
-            ];
-            let mut first = 0;
-            let mut last = 0;
-            let mut first_index = usize::MAX;
-            let mut last_index = usize::MIN;
-
-            for x in n {
-                let a = match x {
-                    "one" | "1" => 1,
-                    "two" | "2" => 2,
-                    "three" | "3" => 3,
-                    "four" | "4" => 4,
-                    "five" | "5" => 5,
-                    "six" | "6" => 6,
-                    "seven" | "7" => 7,
-                    "eight" | "8" => 8,
-                    "nine" | "9" => 9,
-                    _ => 0,
-                };
-                if let Some(p) = line.find(x) {
-                    if first_index > p {
-                        first_index = p;
-                        first = a;
-                    }
-                }
-                if let Some(p) = line.rfind(x) {
-                    if last_index <= p {
-                        last_index = p;
-                        last = a;
-                    }
-                }
-            }
-            first * 10 + last
+            line.replace("one", "o1e")
+                .replace("two", "t2o")
+                .replace("three", "t3e")
+                .replace("four", "f4r")
+                .replace("five", "f5e")
+                .replace("six", "s6x")
+                .replace("seven", "s7n")
+                .replace("eight", "e8t")
+                .replace("nine", "n9e")
         })
-        .sum()
+        .fold(0, |acc, line| {
+            let mut chars = line.chars();
+            let first = chars
+                .find(|x| x.is_numeric())
+                .expect("At least 1 number was expected");
+            let last = chars.rfind(|x| x.is_numeric()).unwrap_or(first);
+            let first = first.to_digit(10).unwrap();
+            let last = last.to_digit(10).unwrap();
+
+            acc + first * 10 + last
+        })
 }
 
 #[cfg(test)]
@@ -82,6 +62,6 @@ mod tests {
     #[test]
     fn test_process_part_2() {
         let input = include_str!("../../inputs/day01_sample_02.txt");
-        assert_eq!(process_part_2(input), 6);
+        assert_eq!(process_part_2(input), 281);
     }
 }
