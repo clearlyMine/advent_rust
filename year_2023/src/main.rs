e6f3db502d8f5ed52d9208e1a1bc67b586f8dccc
clearly mine <clearlymine@pm.me>
2023-12-08 13:14:21 +0530

    add ability to run all days at once

--- year_2023/src/main.rs
+++ year_2023/src/main.rs
@@ -1,3 +1,16 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    for i in 1..=25 {
+        let cmd = Command::new("cargo")
+            .args(["run", "--release", "--bin", format!("day{:02}", i).as_str()])
+            .output()
+            .unwrap();
+        let output = String::from_utf8(cmd.stdout).unwrap();
+        if output.is_empty() {
+            break;
+        }
+        println!("Day {:02}", i);
+        println!("{}", output);
+    }
 }
