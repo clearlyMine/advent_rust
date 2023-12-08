f2accf9b564fa54b7815921ff8d78316ff90d064
clearly mine <clearlymine@pm.me>
2023-12-08 20:48:55 +0530

    optimize solution for 2023-day02

--- year_2023/src/bin/day02.rs
+++ year_2023/src/bin/day02.rs
@@ -6,18 +6,66 @@ use nom::sequence::separated_pair;
 use nom::IResult;
 use std::time::Instant;
 
-fn main() {
-    let input = include_str!("../../inputs/day02.txt");
+#[allow(dead_code)]
+const INPUT: &'static str = include_str!("../../inputs/day02.txt");
+#[allow(dead_code)]
+const SAMPLE: &'static str = include_str!("../../inputs/day02_sample.txt");
 
+fn main() {
     let time_start = Instant::now();
-    let res1 = process_part_1(input);
+    let res1 = process_part_1(INPUT);
     println!("Part 1: {:?}", res1);
     println!("Time: {}μs", time_start.elapsed().as_micros());
 
     let time_start = Instant::now();
-    let res2 = process_part_2(input);
+    let res2 = process_part_2(INPUT);
     println!("Part 2: {:?}", res2);
     println!("Time: {}μs", time_start.elapsed().as_micros());
+}
+
+fn process_part_1(input: &str) -> usize {
+    let (red_limit, blue_limit, green_limit) = (12, 14, 13);
+    input
+        .lines()
+        .map(|line| {
+            let (_, games) = parse_line(line).expect("Unable to parse line");
+            games
+        })
+        .enumerate()
+        .fold(0, |sum, (i, games)| {
+            let (mut min_red_needed, mut min_green_needed, mut min_blue_needed) = (0, 0, 0);
+            for game in games {
+                min_red_needed = min_red_needed.max(game.red);
+                min_blue_needed = min_blue_needed.max(game.blue);
+                min_green_needed = min_green_needed.max(game.green);
+
+                if min_red_needed > red_limit
+                    || min_blue_needed > blue_limit
+                    || min_green_needed > green_limit
+                {
+                    return sum;
+                }
+            }
+            sum + i + 1
+        })
+}
+
+fn process_part_2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let (_, games) = parse_line(line).unwrap();
+            games
+        })
+        .fold(0, |sum, games| {
+            let (mut min_red_needed, mut min_green_needed, mut min_blue_needed) = (0, 0, 0);
+            for game in games {
+                min_red_needed = min_red_needed.max(game.red);
+                min_blue_needed = min_blue_needed.max(game.blue);
+                min_green_needed = min_green_needed.max(game.green);
+            }
+            sum + min_red_needed * min_green_needed * min_blue_needed
+        })
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -31,47 +79,6 @@ impl Game {
     fn new(red: u32, green: u32, blue: u32) -> Game {
         Game { red, green, blue }
     }
-}
-
-fn process_part_1(input: &str) -> usize {
-    input
-        .lines()
-        .enumerate()
-        .map(|(i, line)| {
-            let (_, games) = parse_line(line).unwrap();
-            (i, games)
-        })
-        .fold(0, |mut sum, (i, games)| {
-            let (mut min_red, mut min_green, mut min_blue) = (0, 0, 0);
-            for game in games {
-                min_red = min_red.max(game.red);
-                min_blue = min_blue.max(game.blue);
-                min_green = min_green.max(game.green);
-            }
-            if min_red <= 12 && min_blue <= 14 && min_green <= 13 {
-                sum += i + 1;
-            }
-            sum
-        })
-}
-
-fn process_part_2(input: &str) -> u32 {
-    input
-        .lines()
-        .map(|line| {
-            let (_, games) = parse_line(line).unwrap();
-            games
-        })
-        .map(|games| {
-            let (mut min_red, mut min_green, mut min_blue) = (0, 0, 0);
-            for game in games {
-                min_red = min_red.max(game.red);
-                min_blue = min_blue.max(game.blue);
-                min_green = min_green.max(game.green);
-            }
-            min_red * min_green * min_blue
-        })
-        .sum()
 }
 
 fn parse_line(input: &str) -> IResult<&str, Vec<Game>> {
@@ -101,4 +108,29 @@ fn parse_game(input: &str) -> IResult<&str, Game> {
 
 fn parse_number(input: &str) -> IResult<&str, u32> {
     map_res(digit1, |s: &str| s.parse::<u32>())(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn part_1_sample() {
+        assert_eq!(process_part_1(SAMPLE), 8)
+    }
+
+    #[test]
+    fn part_1_input() {
+        assert_eq!(process_part_1(INPUT), 1931)
+    }
+
+    #[test]
+    fn part_2_sample() {
+        assert_eq!(process_part_2(SAMPLE), 2286)
+    }
+
+    #[test]
+    fn part_2_input() {
+        assert_eq!(process_part_2(INPUT), 83105)
+    }
 }
